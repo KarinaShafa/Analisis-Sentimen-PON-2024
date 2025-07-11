dbaf70c6d54f97348058adaee345e6c73105dc9f
KarinaShafa <karina_shafa_27rpl@student.smktelkom-mlg.sch.id>
2025-07-11 17:41:25 +0700

    update requirement

--- Dashboard/app-pages/page_prediksi_sentimen.py
+++ Dashboard/app-pages/page_prediksi_sentimen.py
@@ -1,8 +1,15 @@
-import re
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 import streamlit as st
-import time
+import re
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 
+import time
+from nltk.corpus import stopwords
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory, StopWordRemover, ArrayDictionary
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import plotly.express as px
+import pandas as pd
 # --------------------------------------
 # Data Processing Code 
 # --------------------------------------
